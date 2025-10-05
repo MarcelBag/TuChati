b6014c6c23033f712848517c1597a7e7850a70dd
Marcel bag <marcelshabani@gmail.com>
2025-10-05 15:25:58 +0200

    workiong on chat init

--- backend/tuchati_config/asgi.py
+++ backend/tuchati_config/asgi.py
@@ -2,8 +2,17 @@
 import os
 import django
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tuchati_config.settings")
 django.setup()
+# Import the websocket_urlpatterns
+from apps.chat.routing import websocket_urlpatterns
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
