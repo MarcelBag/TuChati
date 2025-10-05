8797d63254c7027be92b1ed08d7a8be34af1f501
Marcel bag <marcelshabani@gmail.com>
2025-10-05 16:38:08 +0200

    creating authentication logic with JWT

--- backend/tuchati_config/urls.py
+++ backend/tuchati_config/urls.py
@@ -10,10 +10,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-#from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework_simplejwt.views import TokenRefreshView
 from apps.accounts.views_jwt import CustomTokenObtainPairView
-
 
 urlpatterns = [
     # -------------------------------
@@ -25,7 +23,7 @@ urlpatterns = [
     # -------------------------------
     # JWT Authentication Endpoints
     # -------------------------------
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
     # -------------------------------
