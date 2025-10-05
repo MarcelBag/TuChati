143bf0208428630c6155fd3154081c9b67b89c7c
Marcel bag <marcelshabani@gmail.com>
2025-10-05 14:12:16 +0200

    updating account models

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,10 +1,22 @@
 #backend/apps/accounts/admin.py
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
+
 @admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ("Security", {"fields": ("has_2fa", "totp_secret")}),
+class UserAdmin(BaseUserAdmin):
+    list_display = ("id", "email", "username", "is_staff", "is_online", "date_joined")
+    search_fields = ("email", "username")
+    ordering = ("-date_joined",)
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password", "avatar")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "password1", "password2"),
+        }),
     )
