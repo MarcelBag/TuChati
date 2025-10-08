6dd8961dfbd8839857e0b3a3deea1f2ca4e0c6b1
Marcel bag <marcelshabani@gmail.com>
2025-10-08 23:14:04 +0200

    profile modal

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -1,13 +1,12 @@
 # backend/apps/accounts/urls.py
+from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import RegisterView, MeView
-# import your custom one
 from .views_jwt import CustomTokenObtainPairView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("me/", MeView.as_view(), name="me"),
-    # use custom
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
