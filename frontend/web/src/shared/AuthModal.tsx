0611ed6af41c46eef8451c6d734ade048f822e66
Marcel bag <marcelshabani@gmail.com>
2025-10-09 21:29:59 +0200

    restrictions and routing

--- frontend/web/src/shared/AuthModal.tsx
+++ frontend/web/src/shared/AuthModal.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react'
 import { useAuth } from '../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
-import './auth.css'
+import './AuthModal.css'
 import { apiFetch } from './api'
 
 export default function AuthModal({ onClose }: { onClose: () => void }) {
