4c08ff20c35b49fbe230bb3b4f4f78e6dd3ade03
Marcel bag <marcelshabani@gmail.com>
2025-10-25 19:44:55 +0200

    updating settings

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
-
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = ("id", "email", "username", "is_staff", "is_online", "date_joined")
