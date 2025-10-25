67885dd8d6132e9fd769f708e3940993135cf0f3
Marcel bag <marcelshabani@gmail.com>
2025-10-25 19:28:30 +0200

    updating settings

--- backend/tuchati_config/urls.py
+++ backend/tuchati_config/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     # -------------------------------
     path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-
     # -------------------------------
     # Chat system
     # -------------------------------
